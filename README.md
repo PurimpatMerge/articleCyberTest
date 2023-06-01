4c2a86e59e426bd9c6e994f66b571f75d678c772
“PurimpatMerge” <mergeofficial@hotmail.com>
2023-06-01 15:27:52 +0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # articleCyberTest
+
+ส่งแบบทดสอบ cyber rich digital (backend dev jr)
+
+Installation 
+articleCyberTest/backend command : yarn install
+articleCyberTest/frontend command : yarn install
+import database dbname="article" สามารถ เอา database ผมไป import ได้ครับ จะอยู่ใน articleCyberTest/article.sql
+
+Run
+articleCyberTest/backend command : yarn start
+articleCyberTest/frontend command : yarn dev
+
+Front end
+login page ถ้าไม่สมัครสามารถใช้
+email : mergeofficial@hotmail.com
+password  : 1234Ss
+ผมได้ทำการสร้าง web app แบบ three tires layer ประกอบด้วย web, api and database
+
+จากโจทย์
+1. แยกEnviroment = 
+ผมทำการแยก enviroment ดังต่อไปนี้
+
+-backend/controller เป็นตัวจัดเก็บ ctr ของการใช้งาน routes ต่างๆ  เป็นส่วนที่ทำงานตามความต้องการของการเรียกใช้ ข้อมูล 
+-backend/Routes เป็นตัวรับ จัดการ URL endpoints ตาม method Get,Post,Put,Delete
+-backend/schemes เป็นโครงสร้างของ data ที่จะมาใช้งานกับ datatable นั้นๆ
+-backend/utils ผมทำสำหรับ การ verify กับ ตัว jwt ก่อนใช้งานทรัพยากรบ้างอย่างการที่จะเข้าถึงตัว ctr
+-backend/validation เป็นตัว validation พวก json body ต่างๆก่อนที่จะเข้าไปถึง ctr
+-backend/ctroller/function เนื่องด้วยมีการเรียกใช้งาน การหาข้อมูล reation ในหลายครั้งใน ctr ผมเลยสร้างเป็น function เพื่อเป็นการใช้ซ้ำได้ในการทำงานของ ctr ต่างๆ
+-backend/.env เป็นตัวจัดเก็บ ตัวแปร  variables สำหรับ  environment ที่สำคัญมันจะไม่สามารถมองเห็นได้ในขั้นตอน deploy เราต้อง ไป set เองใน hosting เช่น jwt authenticity , DB_HOST name password ต่างๆ ตัวอย่างเช่น
+process.env.JWT คือ ในหน้า .env จะมี JWT=aJcJWdznlf4BknMudEHeqOMH+g/tex578twTfu+tIS8=***
+   เมื่อเรียกใช้ env  // Generate a JWT token
+    const token = jwt.sign({ userId: results[0].userid}, process.env.JWT, { expiresIn: "7d" });
+
+
+frontend ก็ได้มีการทำการจัดโครง structure แต่ผมขออธิยายในส่วนของ backend ตามบททดสอบ 
+
+///////////////////////////////////////////////////////////////////////////
+
+2.ทำ CRUD = method คืแตาม CRUD:post get put delete 
+ผมได้มีการทำ ไว้ 3 ส่วน route ใน middleware ดังต่อไปนี้
+backend/index.js 
+app.use("/v1/api/users", usersRoute);
+app.use("/v1/api/article", articleRoute);
+app.use("/v1/api/auth", authRoute);
+
+///// controllers/usersRoute /////
+-router.get("/", getUsers); เป็นการเรียกข้อมูลของผู้ใช้งานทั้งหมด
+-router.get("/:id", getUserById); เป็นการหาข้อมูลผ่าน id ของ user นั้นๆ
+-router.post("/register", validationUser, registerUser); เป็นการสมัครโดยการใช้การ bcrypt ให้กับ password โดยการ hash ทำให้มีความ secure 
+-router.put("/update/:id", validationUpdateUser, updateUser); เป็นการ update ของ user นั้นๆดัวย id 
+-router.delete("/:id", deleteUser); เป็นการลบข้อมูลของ user
+
+///// controllers/articleRoute /////
+//Users routes
+-router.post("/createArticle/:id",verifyUser,validationArticle, createArticle); เป็นการสร้างบทความโดยจะต้องมีการ login ก่อนเพราะจะต้องผ่านการ verifyUser jwt หลังจากนั้น จะมีการ validation ตัวแปร json ที่ส่งมา ถึงจะสร้างบทความได้
+-router.put("/updateArticle/:id", validateArticleUpdate, updateArticle); เป็นการ update บทความส่ง id ของบทความนั้นๆมา
+-router.get("/view/:id", getArticleById); เป็นการ ดูบทความและผู้เขียน(รวมถึงขึ้น post)นั้นๆ โดยการ  join on 
+-router.get("/", getAllRelationArticleUser); เป็นการ ดึงข้อมูล แบบ realation table ของ 1.users 2.user_article 3.article โดยใช้การ join on
+-router.get("/getOnlyArticleById/:id", getOnlyArticleById); เป็นการดึง แบบ specfic item สำหรับ บทความนั้นๆ
+-router.get("/table", getAllArticleUser); เป็นการดึงข้แมูล ทั้งหมดแต่ไม่ต้อง join จะได้ข้อมูลที่ต่างกัน และ performance จะเร็วกว่า
+-router.get("/search/", getSearchRelationArticleUser); เป็นการ search item ต่างๆผ่าน keyword ที่หลากหลาย ถ้าดูในโค้ดจะเห็นชัดเจนขึ้นแบบสามารถ ใช้ single input ที่หาได้ผ่านหลาย field 
+-router.delete("/deleteArticle/:id", deleteArticle);เป็นการ delete แบบ realation ใน table user_article และ article นั้นๆ
+-router.post("/addView/:id", incrementArticleView); เป็นการเพิ่มจำนวนยอด view ถ้ากด read more จะ + view ให้ถ้าดูจากหน้าบ้าง
+
+///// controllers/authRoute /////
+
+-router.post("/login", login); เป็นการ login ที่มีการส่ง jwt พร้อมวันหมดอายุ ไปจัดเก็บ ที่ local storage cookie เพื่อทำการนำมาใช้งานใน route createArticle เพื่อที่จะป้องกันการสร้าง จากบุคคลภายนอก
+
+*เพิ่มเติมของการ verfiy สามารถดูได้ที่ function ตรวจสอบที่ verifyUser*
+*เพิ่มเติมการ validation json input (ไม่รวมการ validation ของ user ที่มี email ซ้ำกันนะครับอันนั้นต้องทำแยกที่ register) ดูได้ที่ backend/validaiton*
+
+///////////////////////////////////////////////////////////////////////////
+
+3.แสดงข้อมูล = 
+จาก database จะมี 3 table 1.users 2.user_article 3.article โดย user_article จะจัดเก็บ usersId กับ articleId ไว้จะมีการแสดงได้ดัง page {Home} front-end โดย route ที่สามารถทำได้คือ 1.router.get("/", getAllRelationArticleUser);  2.router.get("/view/:id", getArticleById);
+
+///////////////////////////////////////////////////////////////////////////
+
+4.แสดงรูป สามารถดูได้ที่ front end เลยครับ วิธีจัดเก็บรูปของผมจะใช้ api ของ cloudinary ในการจัดเก็บรูป บน cloud ทำการ save path ในdatabase และ เรียกใช้ ผ่าน link src
+
+///////////////////////////////////////////////////////////////////////////
+
+5.แสดงจำนวนผู้ชม router.post("/addView/:id", incrementArticleView); ในหน้า front end ถ้ากด read more  จะเรียกใช้ axios path นั้เลยครับ จะทำให้+ view ได้ และแสดงก็ดึงข้อมูลได้เลย
+
+///////////////////////////////////////////////////////////////////////////
+6.แสดงจำนวน pagination 
+//////รูปแบบที่ใช้ API ทำ pagination/////////
+router.get("/search/", getSearchRelationArticleUser);
+
+ในหน้า home ที่แสดง article นั้นผมได้ส่ง รับและส่ง ข้อมูล pagination {page} ด้วยการคำนวนเองและใช้ใน front end หน้า home
+ตัวอย่างโค้ด
+const { search, page, limit } = req.query; //โดยจะรับsearch, page (เลขหน้าที่นั้นๆ)จากหน้าบ้านมา พร้อมกับ limit(จำนวนข้อมูลที่ต้องการจะแสดง)
+
+const offset = (page - 1) * limit; //หาข้อมูลจำนวน set นั้นๆ เช่น page เป็น2 ทำการ - 1 จะได้ array index ตัวที่ 1 และ limit 5 ข้อมูลใน หน้า 2 จะเท่ากับข้อมูล 6-10
+
+ทำการ query และ count
+ LIMIT ${limit}
+OFFSET ${offset}
+
+ const totalCount = countResults[0].totalCount;// นับจำนวนทั้งหมด
+ และส่ง totalCount ใน json กลับไปให้ front end คำนวนแสดงว่าทั้งหมดจะมี กี่ page (เช่น totalCount=12 หน้าบ้าง limit ที่ 4 จำนวนเมื่อคำนวนที่ front end จะได้ 3 หน้า)
+//////รูปแบบที่ใช้ Frontend ทำ pagination/////////
+อันนี้สามารถทำได้เลย ผ่าน framwork 
+
+///////////////////////////////////////////////////////////////////////////
+็7.HTTP STATUS 
+ผมได้ทำการตรวจสอบแล้วครับ ใน ทุก ctr แต่ไม่แน่ใจเรื่อง ของdelete response STATUS บ้างที่บอก 200 บ้าง ที่บอก 204 ครับ แต่ส่วนตัวผมใช้ 204ครับจากการวิเคราะห์การใช้งาน ตาม DOC
+
+///////////////////////////////////////////////////////////////////////////
+8.Response json
+ทุกอย่าง res.json หมดครับ
+
+///////////////////////////////////////////////////////////////////////////
+
+อธิบายสิ่งที่ทำ
+จากด้านบนผมได้มีเพิ่มเติมของเรื่อง bearer jwt เข้ามา + validation json input ด้วย Joi + .env + relationDataMapper function + utils การ verfiy jwt ผ่าน header และ cookie+realation database sql (ที่มี table ตัวกลาง รับ id ของ ผู้เขียน กับ บทความ)
---
+++ article.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2023 at 10:23 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `article`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `publishedAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `viewsCount` int(11) DEFAULT 0,
+  `likesCount` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `content`, `author`, `publishedAt`, `updatedAt`, `category`, `tags`, `image`, `viewsCount`, `likesCount`) VALUES
+(19, 'Article 1', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Doe', '2023-05-31 18:21:53', '2023-05-31 18:21:53', 'Technology', 'programming, web development', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 104, 50),
+(43, 'Article 2', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Min game', '2023-06-01 09:34:49', '2023-06-01 13:53:03', 'asfasf', '', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685602384/upload/bzz82txiolegatltgytm.jpg', 1, 0),
+(44, 'Article 37', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'John Smiths', '2023-06-01 09:36:34', '2023-06-01 13:41:45', 'Technology', 'programming, web development', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 251, 150),
+(45, 'Article 12', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Emily Johnson', '2023-06-01 09:36:38', '2023-06-01 13:01:18', 'Health', 'fitness, nutrition', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685599278/upload/v8b6nunvsrjssgj2wacb.jpg', 182, 90),
+(46, 'Article 3', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'David Wilson', '2023-06-01 09:36:44', '2023-06-01 09:36:44', 'Business', 'entrepreneurship, startups', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 320, 200),
+(47, 'Article 4', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Sophia Davis', '2023-06-01 09:36:51', '2023-06-01 09:36:51', 'Science', 'astronomy, biology', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 420, 280),
+(48, 'Article 5', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Michael Thompson', '2023-06-01 09:36:55', '2023-06-01 09:36:55', 'Sports', 'football, basketball', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 800, 400),
+(49, 'Article 6', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Emma Roberts', '2023-06-01 09:37:04', '2023-06-01 09:37:04', 'Technology', 'artificial intelligence, data science', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 150, 75),
+(50, 'Article 7', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Oliver Moore', '2023-06-01 09:37:09', '2023-06-01 09:37:09', 'Health', 'mental health, wellness', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 250, 120),
+(51, 'Article 8', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Liam Wilson', '2023-06-01 09:37:13', '2023-06-01 09:37:13', 'Business', 'marketing, finance', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 380, 240),
+(52, 'Article 9', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Ava Anderson', '2023-06-01 09:37:16', '2023-06-01 09:37:16', 'Science', 'physics, chemistry', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 540, 320),
+(53, 'Article 10', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Sophia Davis', '2023-06-01 09:37:20', '2023-06-01 09:37:20', 'Sports', 'tennis, swimming', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 670, 400),
+(54, 'Article 11', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Jacob Turner', '2023-06-01 09:37:24', '2023-06-01 09:37:24', 'Technology', 'cybersecurity, blockchain', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 320, 180),
+(56, 'Article 13', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'potter', '2023-06-01 09:51:22', '2023-06-01 09:51:22', 'This add the picture', 'This add the picture', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685587882/upload/zh4l1j3daelawexiv13a.jpg', 2, 0),
+(57, 'Article 14', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'devin', '2023-06-01 09:56:30', '2023-06-01 09:56:30', 'Technology', 'programming, web development', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685588189/upload/c6cyyo4f99eaeqhkhxxe.jpg', 1, 0),
+(58, 'Article 15', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'gabay toner', '2023-06-01 10:08:58', '2023-06-01 10:08:58', 'Sports', 'football, basketball', 'https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png', 0, 0),
+(59, 'Article 16', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Marcite', '2023-06-01 10:09:47', '2023-06-01 13:57:11', 'Technology', 'programming, web development', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685602632/upload/nhoovrbg2wsaolbwiccz.jpg', 0, 0),
+(60, 'Article 17', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Homb gave', '2023-06-01 10:11:05', '2023-06-01 10:11:05', 'Technology', 'cybersecurity, blockchain', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685589065/upload/obfnjzorrzjco27copid.jpg', 0, 0),
+(61, 'Article 18', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:', 'Steab', '2023-06-01 10:33:05', '2023-06-01 10:33:05', 'Sports', 'football, basketball', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685590384/upload/hzpvkksxclpjzdpxysqh.jpg', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `uemail` varchar(255) NOT NULL,
+  `upassword` varchar(255) NOT NULL,
+  `upicture` text NOT NULL,
+  `updateAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `fname`, `lname`, `username`, `uemail`, `upassword`, `upicture`, `updateAt`) VALUES
+(6, 'pruimpat', 'worakaicharuphak', 'miniheart', 'mergeofficial@hotmail.com', '$2a$10$JQV3mZiOX6RL.l.Gp3ZZqORjNE.7vw/AZTmIzPeg5grzOf/Ixo1V6', 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80', '2023-06-01 06:30:57'),
+(8, 'kea', 'mattaya', 'Mergeonlife', 'mergeofficial2@hotmail.com', '$2a$10$cLUcWy62BUXQZ4LYL8Oqm.wptzVQ0Yr3FzyQbVMnB1gjSkVdBupm.', 'https://i.pinimg.com/474x/4c/3e/3b/4c3e3b91f05a5765aa544ac7557d6642.jpg', '2023-06-01 06:31:00'),
+(11, 'shima', 'potter', 'grand', 'mdnj@hotmail.com', '$2a$10$fIOAPFZq/3yUiGOos6hosu0SLFsppfZPIQgcdGksrcMaLNYdHj2KG', 'http://res.cloudinary.com/dwwfqdl79/image/upload/v1685590102/upload/cigpmyu3da5wk5mryaa2.jpg', '2023-06-01 03:41:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_articles`
+--
+
+CREATE TABLE `user_articles` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `articleId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_articles`
+--
+
+INSERT INTO `user_articles` (`id`, `userId`, `articleId`) VALUES
+(19, 11, 19),
+(43, 8, 43),
+(44, 11, 44),
+(45, 8, 45),
+(46, 6, 46),
+(47, 11, 47),
+(48, 6, 48),
+(49, 8, 49),
+(50, 8, 50),
+(51, 11, 51),
+(52, 6, 52),
+(53, 8, 53),
+(54, 11, 54),
+(56, 11, 56),
+(57, 8, 57),
+(58, 11, 58),
+(59, 8, 59),
+(60, 6, 60),
+(61, 11, 61);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `uemail` (`uemail`),
+  ADD UNIQUE KEY `uemail_3` (`uemail`),
+  ADD UNIQUE KEY `uemail_4` (`uemail`),
+  ADD KEY `uemail_2` (`uemail`);
+
+--
+-- Indexes for table `user_articles`
+--
+ALTER TABLE `user_articles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `articleId` (`articleId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user_articles`
+--
+ALTER TABLE `user_articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_articles`
+--
+ALTER TABLE `user_articles`
+  ADD CONSTRAINT `user_articles_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userid`),
+  ADD CONSTRAINT `user_articles_ibfk_2` FOREIGN KEY (`articleId`) REFERENCES `articles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
