f12f8f25a2f2ef8f7f8ddfbdd5f86d8946a65e05
“PurimpatMerge” <mergeofficial@hotmail.com>
2023-06-01 10:12:01 +0700

    successly create done for article

--- frontend/src/components/addarticle/Addarticleinput.jsx
+++ frontend/src/components/addarticle/Addarticleinput.jsx
@@ -1,50 +1,76 @@
-import Divider from '@mui/material/Divider';
-import { Image } from 'antd';
-import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import { useState, useEffect } from "react";
-import TextField from '@mui/material/TextField';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
-import Button from '@mui/material/Button';
+import Divider from "@mui/material/Divider";
+import { Image } from "antd";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import TextareaAutosize from "@mui/base/TextareaAutosize";
+import Button from "@mui/material/Button";
 import axios from "axios";
 
 const Addarticleinput = () => {
-  const [imageURLs, setImageURLs] = useState([]);
   const [images, setImages] = useState([]);
-  const [info, setInfo] = useState({});
-  
-  useEffect(() => {
-    if (images.length < 1) return;
-    const newImageUrls = [];
-    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image)));
-    setImageURLs(newImageUrls);
-  }, [images]);
+  const [info, setInfo] = useState({
+    image: "https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png",
+  });
 
   const onImageChange = (e) => {
-    setImages([...e.target.files]);
+    const file = e.target.files[0];
+    setImages([file]);
+    setInfo((prev) => ({
+      ...prev,
+      image: URL.createObjectURL(file),
+    }));
   };
+
   const handleChange = (e) => {
     setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
-const allInfo={
-...info,
-images
-}
   const handleSubmit = async () => {
     try {
-      console.log(allInfo);
-      const userid = localStorage.getItem('userid');
-      console.log(userid);
+      let list = [];
   
-      const accessToken = localStorage.getItem('accessToken'); // Retrieve the access token from local storage
+      if (images.length > 0) {
+        list = await Promise.all(
+          images.map(async (file) => {
+            const data = new FormData();
+            data.append("file", file);
+            data.append("upload_preset", "upload");
   
-      await axios.post(`http://localhost:8000/v1/api/article/createArticle/${userid}`, allInfo, {
-        headers: {
-          Authorization: `Bearer ${accessToken}` // Include the access token in the request headers
+            const uploadRes = await axios.post(
+              "https://api.cloudinary.com/v1_1/dwwfqdl79/image/upload",
+              data
+            );
+  
+            const { url } = uploadRes.data;
+            return url;
+          })
+        );
+      } else {
+        // Set a default image URL if no image is selected
+        list = ["https://contenthub-static.grammarly.com/blog/wp-content/uploads/2022/08/BMD-3398.png"];
+      }
+  
+      const allInfo = {
+        ...info,
+        tags: [info.tags],
+        image: list,
+      };
+  
+      const userid = localStorage.getItem("userid");
+  
+      const accessToken = localStorage.getItem("accessToken");
+  
+      await axios.post(
+        `http://localhost:8000/v1/api/article/createArticle/${userid}`,
+        allInfo,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
         }
-      });
-  
+      );
     } catch (err) {
       console.log("You're not logged in.");
     }
@@ -55,54 +81,96 @@ images
     <div className="container mx-auto my-10 bg-white p-5 rounded-sm shadow-xl">
       {/* Header */}
       <div>
-        <p className='text-4xl'>Creat Your Article</p>
+        <p className="text-4xl">Create Your Article</p>
       </div>
       <Divider />
       {/* Upload Image*/}
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <div>
-          <IconButton color="primary" aria-label="upload picture" component="label">
-            <input hidden accept="image/*" type="file" onChange={onImageChange} multiple />
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="label"
+          >
+            <input
+              hidden
+              accept="image/*"
+              type="file"
+              onChange={onImageChange}
+              multiple
+            />
             <PhotoCamera />
           </IconButton>
           <label className="text-blue-500">Upload Image</label>
         </div>
         <div>
-          {imageURLs.map((imageSrc, index) => (
-            <Image key={index} height={400} src={imageSrc} alt="profileimg" />
-          ))}
+          {info.image && (
+            <Image
+              height={400}
+              src={info.image}
+              alt="profileimg"
+              onChange={(e) => setImages(e.target.files)}
+            />
+          )}
         </div>
       </div>
       {/* add Header */}
-      <div className='my-1'>
+      <div className="my-1">
         <p>Title</p>
-        <TextField variant="outlined" className='w-full' id="title"   onChange={handleChange} />
+        <TextField
+          variant="outlined"
+          className="w-full"
+          id="title"
+          onChange={handleChange}
+        />
       </div>
       <Divider />
       {/* add content */}
-      <div className='my-1'>
+      <div className="my-1">
         <p>CONTENT</p>
-        <TextareaAutosize className='w-full border py-5 border-gray-200 rounded-md' label="Header" variant="standard" id="content"  onChange={handleChange} />
+        <TextareaAutosize
+          className="w-full border py-5 border-gray-200 rounded-md"
+          label="Header"
+          variant="standard"
+          id="content"
+          onChange={handleChange}
+        />
       </div>
       {/* add author */}
-      <div className='my-1'>
+      <div className="my-1">
         <p>AUTHOR</p>
-        <TextField variant="outlined" className='w-full' id="author"  onChange={handleChange} />
+        <TextField
+          variant="outlined"
+          className="w-full"
+          id="author"
+          onChange={handleChange}
+        />
       </div>
       {/* add category */}
-      <div className='my-1'>
+      <div className="my-1">
         <p>CATEGORY</p>
-        <TextField variant="outlined" className='w-full' id="category"   onChange={handleChange} />
+        <TextField
+          variant="outlined"
+          className="w-full"
+          id="category"
+          onChange={handleChange}
+        />
       </div>
       {/* add tags */}
-      <div className='my-1'>
+      <div className="my-1">
         <p>TAGS (comma-separated)</p>
-        <TextField variant="outlined" className='w-full' id="tags" onChange={handleChange} />
-
+        <TextField
+          variant="outlined"
+          className="w-full"
+          id="tags"
+          onChange={handleChange}
+        />
       </div>
       {/* button */}
-      <div className='flex justify-end'>
-        <Button variant="contained" onClick={handleSubmit}>Apply</Button>
+      <div className="flex justify-end">
+        <Button variant="contained" onClick={handleSubmit}>
+          Apply
+        </Button>
       </div>
     </div>
   );
